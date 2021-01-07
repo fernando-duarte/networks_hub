b5147f11a80d6c256d10a913e18c611f806bbc97
Fernando Duarte <duarte@alum.mit.edu>
2021-01-07 15:07:10 +0000

    benchmark.jl now runs
    With N=2, it takes less than 10 minutes (including package loading and compile times)

--- SAN/code/network_simulations/benchmark.jl
+++ SAN/code/network_simulations/benchmark.jl
@@ -1,12 +1,46 @@
-using LinearAlgebra, DataFrames, XLSX, Missings, JuMP, Ipopt, Random, Complementarity, Test, Distributions, DistributionsAD, SpecialFunctions, Quadrature, NLsolve
-using Quadrature, ForwardDiff, FiniteDiff, Zygote, Cuba, DistributionsAD, Cubature, HCubature
+using Pkg
+Pkg.build("HDF5")
+Pkg.add("JLD")
+Pkg.add("SpecialFunctions")
+Pkg.build("SpecialFunctions")
+Pkg.add("FFTW")
+Pkg.build("FFTW")
+Pkg.add("PATHSolver")
+Pkg.build("PATHSolver")
+Pkg.add("SLEEFPirates")
+Pkg.build("SLEEFPirates")
+Pkg.add("IterTools")
+Pkg.add("Tracker")
 
-N = 20 # keep largest `N' nodes by assets
+# restart kernel after running the lines above
+using Pkg
+Pkg.add("DataFrames")
+Pkg.add("XLSX")
+Pkg.add("Missings")
+Pkg.add("JuMP")
+Pkg.add("Ipopt")
+Pkg.add("Complementarity")
+Pkg.add("DistributionsAD")
+Pkg.add("Zygote")
+Pkg.add(Pkg.PackageSpec(url="https://github.com/SciML/Quadrature.jl", rev="master"))
+Pkg.add(Pkg.PackageSpec(url="https://github.com/JuliaDiff/ForwardDiff.jl", rev="master"))
+Pkg.add(Pkg.PackageSpec(url="https://github.com/JuliaDiff/FiniteDiff.jl", rev="master"))
+Pkg.add(Pkg.PackageSpec(url="https://github.com/giordano/Cuba.jl", rev="master"))
+Pkg.add("Cubature")
+Pkg.add("HCubature")
+Pkg.add("PlotlyBase");Pkg.add("PlotlyJS"); Pkg.add("ORCA")
+ 
+using LinearAlgebra, DataFrames, XLSX, Missings, JuMP, Ipopt, Random, Complementarity, Test, Distributions, DistributionsAD, SpecialFunctions, NLsolve
+using Quadrature, ForwardDiff, FiniteDiff, Zygote, Cuba, Cubature, HCubature
+using Plots
+plotly(ticks=:native)  
+
+N = 2 # keep largest `N' nodes by assets
 
 ## load data
 xf = XLSX.readxlsx("node_stats_forsimulation_all.xlsx") 
-data = vcat( [(XLSX.eachtablerow(xf[s]) |> DataFrames.DataFrame) for s in XLSX.sheetnames(xf)]... )
-unique!(data) # delete duplicate rows, use `nonunique(data)' to see if there are any duplicates
+data = vcat( [(XLSX.eachtablerow(xf[s]) |> DataFrames.DataFrame) for s in XLSX.sheetnames(xf)]... ) #for s in XLSX.sheetnames(xf) if (s!="Aggregates Composition" && s!="Dealer Aggregates" && s!="Approx Aggregates")
+unique!(data) # delete duplicate rows, use `nonunique(data)` to see if there are any duplicates
 data = data[isequal.(data.qt_dt,195), :] # keep quarter == 195 = 2008q4
 sort!(data, :assets, rev = true)
 data = data[1:N,:] # keep small number of nodes, for testing
@@ -24,7 +58,7 @@ dropmissing(data, [:delta, :delta_alt, :w, :assets, :p_bar]) # remove type missi
 
 names(data) # column names
 describe(data)
-show(data, true)
+show(data, allcols = true)
 
 p_bar = data.p_bar
 assets = data.assets
@@ -32,6 +66,7 @@ delta = data.delta
 w= data.w
 g0 = 0.05   # bankruptcy cost
 
+# initial guess
 rng =  Random.seed!(123)
 A0 = zeros(N,N)
 c0 =  data_nm.c 
@@ -47,10 +82,13 @@ for i=1:N
     else
         sol = nlsolve(ff!,[β0[i-1]])
     end
+    @test converged(sol)
     push!(β0,sol.zero[1])
 end
 
 dist_pdf(x,p) = exp(DistributionsAD.logpdf(DistributionsAD.Beta(p[1],p[2]),x[1]))
+Plots.plot([x->dist_pdf(x,[α0[i],β0[i]]) for i=1:N],0,1)
+
 function dist_cdf(p...)
     α,β,wc = p[1], p[2], p[3]
     s(x,wc) = 1 ./(1 .+exp.(-1000*(x-wc))) # approximates indicator function of x>=wc
@@ -61,7 +99,7 @@ end
 
 function dist_pdf_mv(x,params)
            # α,β,A,b,c = params[1][1:N], params[1][N+1:2*N], params[1][2*N+1:2*N+N^2], params[1][2*N+N^2+1:3*N+N^2], params[1][3*N+N^2+1:4*N+N^2]
-        α,β,A,b,c = params[1:N], params[N+1:2*N], params[2*N+1:2*N+N^2], params[2*N+N^2+1:3*N+N^2], params[3*N+N^2+1:4*N+N^2]
+            α,β,A,b,c = params[1:N], params[N+1:2*N], params[2*N+1:2*N+N^2], params[2*N+N^2+1:3*N+N^2], params[3*N+N^2+1:4*N+N^2]
             A = reshape([A...],N,N) 
             joint_pdf = 1.0;
             for i=1:N
@@ -75,32 +113,34 @@ function dist_pdf_mv(x,params)
             loss_x(x) = -sum(contraction_iter(x,2)).*dist_pdf(x,[α...,β...])   
             loss_x(x)*joint_pdf
 end
-function test(x,params...)
-    α,β,A,b,c = params[1][1:N], params[1][N+1:2*N], params[1][2*N+1:2*N+N^2], params[1][2*N+N^2+1:3*N+N^2], params[1][3*N+N^2+1:4*N+N^2]
-    params
-end
-test(ones(N)/5,ic)
 
-function dist_pdf_mv(x,params)
-    α,β,A,b,c = params[1:N], params[N+1:2*N], params[2*N+1:2*N+N^2], params[2*N+N^2+1:3*N+N^2], params[3*N+N^2+1:4*N+N^2]
-    A = reshape([A...],N,N) 
-    joint_pdf = prod(dist_pdf.(x,[α,β]))
-    sum([α...,β...,A...,b...,c...])*joint_pdf
-end
+
+# function dist_pdf_mv_test(x,params...)
+#     α,β,A,b,c = params[1:N], params[N+1:2*N], params[2*N+1:2*N+N^2], params[2*N+N^2+1:3*N+N^2], params[3*N+N^2+1:4*N+N^2]
+#     A = reshape([A...],N,N) 
+#     joint_pdf=1.0;
+#     for i=1:N
+#         joint_pdf = joint_pdf*dist_pdf(x[i],[α[i],β[i]]) 
+#     end
+#     #joint_pdf = prod(dist_pdf.(x,[α,β]))
+#     some_fun = x->sum(x) # some arbitrary function to test
+#     some_fun([α...,β...,A...,b...,c...])*joint_pdf 
+# end
+# dist_pdf_mv_test(ones(N)/12.0,ic...)
+
 function ev(params...)
     prob = QuadratureProblem(dist_pdf_mv,zeros(N),ones(N),[params...])
     Quadrature.solve(prob,CubaCuhre(),reltol=1e-5,abstol=1e-5)[1]
 end
 
 ic = [α0...,β0...,A0...,b0...,c0...]
-dist_pdf_mv(ones(N)/1.0,[(ic...,)...])
+dist_pdf_mv(ones(N)/10.0,ic)
 ev(ic...)
-
-
 
 # set up optimization
 # m = Model(with_optimizer(Ipopt.Optimizer, start_with_resto="yes", linear_solver="mumps",max_iter=10000,hessian_approximation="limited-memory"))
-m = Model(with_optimizer(Ipopt.Optimizer,max_iter=100000,print_level=6))
+m = Model(optimizer_with_attributes(Ipopt.Optimizer,"max_iter"=>100000,"print_level"=>5))
+
 @variable(m, 0.8<=α[i=1:N]<=3.0, start = α0[i]) 
 @variable(m, 1.0<=β[i=1:N]<=150.0, start = β0[i]) 
 #@variable(m, 0<=p[i=1:N,j=1:D]<=data.p_bar[i], start = data.p_bar[i]) 
@@ -148,17 +188,11 @@ st0 = termination_status(m)
 obj0 = objective_value(m)
 @show st0, obj0
 
-psol0 = JuMP.value.(p)
 csol0 = JuMP.value.(c)
 bsol0 = JuMP.value.(b)
 Asol0 = JuMP.value.(A)
 αsol0 = JuMP.value.(α)
 βsol0 = JuMP.value.(β)
-zsol0 = [Asol0...;bsol0...;csol0...;psol0...]
-Zsol0 = zsol0*zsol0'
-rank(Zsol0)
-eigvals(Zsol0)
-
 
 cdf_pkg = [1-cdf(Beta(αsol0[i],βsol0[i]),w[i]/csol0[i]) for i=1:N]
 cdf_opt = [dist_cdf([αsol0[i],βsol0[i],w[i]/csol0[i] ]...) for i=1:N]
@@ -170,17 +204,14 @@ tol = 1e-5
     @test norm([Asol0[i,j]*Asol0[j,i] for i=1:N , j=1:N]) < tol
     @test all(0 .<=Asol0.<=1)
     @test all(0 .<=bsol0.<=data.p_bar)
-    @test all(0 .<=csol0.<=data.assets)
-    @test all(0 .<=psol0.<=data.p_bar)
-    
-    @test norm(min.(p_bar,max.((1+g0).*(Asol0'*psol0[:,1] .+ csol0 .- x0[:,1].*csol0 ) .- g0.*p_bar,0))-psol0[:,1])<tol
-    
+    @test all(0 .<=csol0.<=data.assets)   
 
     @test norm(cdf_pkg-cdf_opt)<0.01
     @test norm(delta-cdf_opt)<0.01
     
 end
 
+#=
 function clearing_vec!(F, p, x)
     F .= p.-min.(p_bar,max.((1+g0)*(Asol0'*p .+csol0 .- x.*csol0) .-g0.*p_bar,0))
 end
@@ -209,7 +240,7 @@ ratio = L_opt/L_d
 bound = 1 + sum(delta.*csol0)/((1-β⁺)*sum(csol0))
 @show (ratio,bound)
 
-
+=#
 
 
 
@@ -251,6 +282,8 @@ bound = 1 + sum(delta.*csol0)/((1-β⁺)*sum(csol0))
 # cdf_opt = [dist_cdf([a1[i],b1[i],w[i]/c1[i] ]...) for i=1:N]
 # @test norm(cdf_pkg-cdf_opt)<0.01
 # @test norm(delta-cdf_opt)<0.01
+
+#=
 
 
 using SparseGrids, FastGaussQuadrature
@@ -330,3 +363,6 @@ dist_pdf_mv(ones(N)/20,[ic...])
 ev(ic...)
 ev(ic)
 dev = ForwardDiff.gradient(dist_pdf_mv,params)
+
+
+=#
