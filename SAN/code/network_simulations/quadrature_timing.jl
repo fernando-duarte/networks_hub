e9e46e82c17482ac56f651028b522fdb7f69687c
smitch151 <52943255+smitch151@users.noreply.github.com>
2021-03-25 16:25:24 -0400

    Adding quadrature solutions for dirichlet
    However, the solutions don't match those provided by wikipedia. I think the reason is because the support for quadrature doesn't match with the theoretical one. 
    
    Dirichlet is defined for x where sum(x) == 1. But, the logpdf of a Dirichlet is positive valued for examples of x where sum(x) != 1.

--- SAN/code/network_simulations/quadrature_timing.jl
+++ SAN/code/network_simulations/quadrature_timing.jl
@@ -22,84 +22,64 @@ using DistributionsAD: TuringDirichlet
 Random.seed!(123)
 
 ## Number of nodes
-N =2 
+N = 2
 # N = prase(Int64, ARGS[1]) - if called via bash 
 
 #User Inputs
 α0 = 10 .* (1 .- rand(N))
 β0 = 50 .* (1 .- rand(N))
-reltol_val = 1e-5
-abstol_val = 1e-5
+reltol_val = 1e-3
+abstol_val = 1e-3
 alg = HCubatureJL()
 batch_val = 0
 parallel_val = 0
 time_start = time()
 mem_start = Sys.free_memory()/1000/2^20
 
-# is this broken? 
-rand(DistributionsAD.TuringDirichlet(N, 1.0),5) 
-@testset "TuringDirichlet" begin
-        dim = 3
-        n = 4
-        for alpha in (2, rand())
-            d1 = TuringDirichlet(dim, alpha)
-            d2 = Dirichlet(dim, alpha)
-            #d3 = TuringDirichlet(d2)
-            @test d1.alpha == d2.alpha # == d3.alpha
-            @test d1.alpha0 == d2.alpha0 # == d3.alpha0
-            @test d1.lmnB == d2.lmnB # == d3.lmnB
-
-            s1 = rand(d1)
-            @test s1 isa Vector{Float64}
-            @test length(s1) == dim
-
-            s2 = rand(d1, n)
-            @test s2 isa Matrix{Float64}
-            @test size(s2) == (dim, n)
-        end
-
-        for alpha in (ones(Int, dim), rand(dim))
-            d1 = TuringDirichlet(alpha)
-            d2 = Dirichlet(alpha)
-            #d3 = TuringDirichlet(d2)
-            @test d1.alpha == d2.alpha # == d3.alpha
-            @test d1.alpha0 == d2.alpha0 # == d3.alpha0
-            @test d1.lmnB == d2.lmnB # == d3.lmnB
-
-            s1 = rand(d1)
-            @test s1 isa Vector{Float64}
-            @test length(s1) == dim
-
-            s2 = rand(d1, n)
-            @test s2 isa Matrix{Float64}
-            @test size(s2) == (dim, n)
-        end
-end
-DistributionsAD.logpdf(TuringDirichlet([1.2,3.4,5.1]),[0.1,0.2,0.5])
-
-Zygote.gradient( (alpha1,alpha2,alpha3) -> exp(DistributionsAD.logpdf(TuringDirichlet([alpha1,alpha2,alpha3]),[0.1,0.2,0.5])) , (1.2,1.3,1.5)...)
-
-zg(a1,a2,x1,x2) = Zygote.gradient( (alpha1,alpha2)-> exp(DistributionsAD.logpdf(TuringDirichlet([alpha1,alpha2]),[x1,x2])) , (a1,a2)...)
-zg(1.3,1.5,0.45,0.12)
-
-using SpecialFunctions
-pdfDirichlet(a1,a2,x1,x2) = exp(DistributionsAD.logpdf(TuringDirichlet([a1,a2]),[x1,x2]))
-mg(a1,a2,x1,x2)=pdfDirichlet(a1,a2,x1,x2)*(digamma(a1+a2)-digamma(a1)+log(x1))
-zg(1.3,1.5,0.45,0.12)
-mg(1.3,1.5,0.45,0.12)
-
-for alpha in (ones(Int, 3), rand(3))
-    display(alpha)
-end
-
 ## Dirichlet distribution
-dist_pdf(x,p) = exp(DistributionsAD.logpdf(DistributionsAD.TuringDirichlet(N,p[1]),x[1]))
-f(x,p) = dist_pdf(x,p) .* x
-prob = QuadratureProblem(f,0.0,1.0,[α0[1]...])
-sol = Quadrature.solve(prob,alg,reltol=reltol_val,abstol=abstol_val)[1]
+function dist_dirichlet_pdf_corrected(x,p)
+    if sum(x) - 1.00 < 1e6
+        exp(DistributionsAD.logpdf(DistributionsAD.TuringDirichlet(p),x)) 
+    else
+        0.0
+    end
+end
 
+# Mean
+dist_dirichlet_pdf(x,p) = exp(DistributionsAD.logpdf(DistributionsAD.TuringDirichlet(p),x)) 
+f(x,p) = dist_dirichlet_pdf(x,p) * sum(x)
+prob = QuadratureProblem(f,zeros(N),ones(N), α0)
+#prob = QuadratureProblem(f, zeros(N) , [1000.0, 1000.0], α0)
+sol_mean = Quadrature.solve(prob,alg,reltol=reltol_val,abstol=abstol_val)
+
+mean_dirichlet(p) = p./sum(p)
+
+mean_dirichlet(α0)
+
+# Variance
+dist_dirichlet_pdf(x,p) = exp(DistributionsAD.logpdf(DistributionsAD.TuringDirichlet(p),x)) 
+f(x,p) = dist_dirichlet_pdf(x,p) * sum(((x .- mean_dirichlet(α0))).^2)
+prob = QuadratureProblem(f,zeros(N),ones(N), α0)
+#prob = QuadratureProblem(f, zeros(N) , [1000.0, 1000.0], α0)
+sol_var = Quadrature.solve(prob,alg,reltol=reltol_val,abstol=abstol_val)
+
+var_dirichlet(p) = ((p./sum(p)) .- (p./sum(p)).^2) ./(sum(p)+1)
+var_dirichlet(α0)
+
+covar_dirichlet(p, i, j) = -p[i]*p[j]/(sum(p)^3 + sum(p))   #only when i != j
+covar_dirichlet(α0,2,1)
+
+# Mean of Sqrt
+dist_dirichlet_pdf(x,p) = exp(DistributionsAD.logpdf(DistributionsAD.TuringDirichlet(p),x)) 
+f(x,p) = dist_dirichlet_pdf(x,p) * sum(sqrt.(x))^2
+prob = QuadratureProblem(f,zeros(N),ones(N), α0)
+#prob = QuadratureProblem(f, zeros(N) , [1000.0, 1000.0], α0)
+sol_sqrt = Quadrature.solve(prob,alg,reltol=reltol_val,abstol=abstol_val)
+
+#=
 # Matrix Beta
-dist_pdf(x,p) = exp(DistributionsAD.logpdf(DistributionsAD.MatrixBeta(N,p[1],p[2]),x[1]))
+
+dist_pdf(x,p) = exp(DistributionsAD.logpdf(DistributionsAD.MatrixBeta(N,p[1],p[2]),x))
 f(x,p) = dist_pdf(x,p) .* x
 prob = QuadratureProblem(f,0.0,1.0,[α0[1]..., β0[1]...])
 sol = Quadrature.solve(prob,alg,reltol=reltol_val,abstol=abstol_val)[1]
@@ -158,3 +138,62 @@ data_out.computation_time_sec = total_time
 data_out.maxRAM_used_gb = total_mem
 
 CSV.write("quadrature_results.csv",data_out)
+
+## Tests
+# is this broken? 
+w = rand(DistributionsAD.TuringDirichlet(α0),5) 
+
+@testset "TuringDirichlet" begin
+        dim = 3
+        n = 4
+        for alpha in (2, rand())
+            d1 = TuringDirichlet(dim, alpha)
+            d2 = Dirichlet(dim, alpha)
+            #d3 = TuringDirichlet(d2)
+            @test d1.alpha == d2.alpha # == d3.alpha
+            @test d1.alpha0 == d2.alpha0 # == d3.alpha0
+            @test d1.lmnB == d2.lmnB # == d3.lmnB
+
+            s1 = rand(d1)
+            @test s1 isa Vector{Float64}
+            @test length(s1) == dim
+
+            s2 = rand(d1, n)
+            @test s2 isa Matrix{Float64}
+            @test size(s2) == (dim, n)
+        end
+
+        for alpha in (ones(Int, dim), rand(dim))
+            d1 = TuringDirichlet(alpha)
+            d2 = Dirichlet(alpha)
+            #d3 = TuringDirichlet(d2)
+            @test d1.alpha == d2.alpha # == d3.alpha
+            @test d1.alpha0 == d2.alpha0 # == d3.alpha0
+            @test d1.lmnB == d2.lmnB # == d3.lmnB
+
+            s1 = rand(d1)
+            @test s1 isa Vector{Float64}
+            @test length(s1) == dim
+
+            s2 = rand(d1, n)
+            @test s2 isa Matrix{Float64}
+            @test size(s2) == (dim, n)
+        end
+end
+
+
+## Turing Dirichlet 
+DistributionsAD.logpdf(TuringDirichlet([1.2,3.4,5.1]),[0.1,0.2,0.5])
+
+Zygote.gradient( (alpha1,alpha2,alpha3) -> exp(DistributionsAD.logpdf(TuringDirichlet([alpha1,alpha2,alpha3]),[0.1,0.2,0.5])) , (1.2,1.3,1.5)...)
+
+zg(a1,a2,x1,x2) = Zygote.gradient( (alpha1,alpha2)-> exp(DistributionsAD.logpdf(TuringDirichlet([alpha1,alpha2]),[x1,x2])) , (a1,a2)...)
+zg(1.3,1.5,0.45,0.12)
+
+using SpecialFunctions
+pdfDirichlet(a1,a2,x1,x2) = exp(DistributionsAD.logpdf(TuringDirichlet([a1,a2]),[x1,x2]))
+mg(a1,a2,x1,x2)=pdfDirichlet(a1,a2,x1,x2)*(digamma(a1+a2)-digamma(a1)+log(x1))
+zg(1.3,1.5,0.45,0.12)
+mg(1.3,1.5,0.45,0.12)
+
+=#
