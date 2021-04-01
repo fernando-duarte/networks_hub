06a4dadae76ff86c077798c213f49b83b2e6dcb1
smitch151 <52943255+smitch151@users.noreply.github.com>
2021-04-01 00:04:25 -0400

    Updating for new joint_timing.toml files

--- SAN/code/network_simulations/run_batch_quadrature.sh
+++ SAN/code/network_simulations/run_batch_quadrature.sh
@@ -4,7 +4,7 @@
 # third argument $3 is the number of nodes
 
 # folder to save results
-OUT_DIR="./quadrature_timing/"
+OUT_DIR="./quadrature_results/"
 mkdir -p $OUT_DIR
 
 # setting up access (only needed on first use)
@@ -12,8 +12,18 @@ aws s3 cp s3://ran-s3-install-pkgs/config/RanPocKP.pem .
 chmod 400 RanPocKP.pem
 
 # copy package environment
-aws s3 cp /home/ec2-user/SageMaker/networks_hub/SAN/code/network_simulations/quadrature_timing/Project.toml "$2"quadrature_timing/Project.toml
-aws s3 cp /home/ec2-user/SageMaker/networks_hub/SAN/code/network_simulations/quadrature_timing/Manifest.toml "$2"quadrature_timing/Manifest.toml
+aws s3 cp  /home/ec2-user/SageMaker/networks_hub/SAN/code/network_simulations/joint_timing/Project.toml "$2"Project.toml
+aws s3 cp  /home/ec2-user/SageMaker/networks_hub/SAN/code/network_simulations/joint_timing/Manifest.toml "$2"Manifest.toml
+aws s3 cp  /home/ec2-user/SageMaker/networks_hub/SAN/code/network_simulations/joint_timing/src/joint_timing.jl "$2"src/joint_timing.jl
+
+
+mkdir -p /home/ec2-user/joint_timing
+mkdir -p /home/ec2-user/joint_timing/src
+
+
+cp /home/ec2-user/SageMaker/networks_hub/SAN/code/network_simulations/joint_timing/Project.toml /home/ec2-user/joint_timing/Project.toml
+cp /home/ec2-user/SageMaker/networks_hub/SAN/code/network_simulations/joint_timing/Manifest.toml /home/ec2-user/joint_timing/Manifest.toml
+cp /home/ec2-user/SageMaker/networks_hub/SAN/code/network_simulations/joint_timing/src/joint_timing.jl /home/ec2-user/joint_timing/src/joint_timing.jl
 
 # copy julia script to s3
 aws s3 cp "$1" "$2"
@@ -23,22 +33,23 @@ ssh -o StrictHostKeyChecking=no -i RanPocKP.pem ec2-user@172.31.100.6 <<ENDSSH
 aws s3 cp "$2""$1" ~/"$1"
 # copy package environment
 mkdir -p quadrature_timing
-aws s3 cp "$2"quadrature_timing quadrature_timing/ --recursive 
+#aws s3 cp "$2"quadrature_timing quadrature_timing/ --recursive 
 #nohup time ~/julia-1.6.0/bin/julia  ~/"$1">output_"$1".txt
 #nohup time env JULIA_NUM_THREADS=4 julia ~/"$1">output_"$1".txt # 4 threads
 #nohup time ~/julia-1.6.0/bin/julia  -p 4 ~/"$1">output_"$1".txt  # 4 workers
 nohup time julia ~/"$1">output_"$3"_"$1".txt $3 # calling julia
-aws s3 cp /home/ec2-user/output_"$3"_"$1".txt "$2"output_"$3"_"$1".txt
-aws s3 cp /home/ec2-user/meta_data"$3".csv "$2"meta_data_"$3".csv
+aws s3 cp /home/ec2-user/output_"$3"_"$1".txt "$2"quadrature_results/output_"$3"_"$1".txt
+aws s3 cp /home/ec2-user/meta_data_"$3".csv "$2"quadrature_results/meta_data_"$3".csv
 
 ENDSSH
 ENDSUDO
 
 # copy output from s3 to local
-aws s3 cp "$2"output_"$3"_"$1".txt $OUT_DIR
-aws s3 cp "$2"meta_data_"$3".csv $OUT_DIR
+aws s3 cp "$2"quadrature_results/output_"$3"_"$1".txt $OUT_DIR
+aws s3 cp "$2"quadrature_results/meta_data_"$3".csv $OUT_DIR
 
 # remove julia script and output files from s3
 # aws s3 rm "$2""$1"
 # aws s3 rm "$2"output_"$1".txt
 # aws s3 rm "$2"/* --recursive 
+
