e041f2f8f6f1c0cebe65c538ae48e42f83f2521b
smitch151 <52943255+smitch151@users.noreply.github.com>
2021-04-01 00:05:10 -0400

    Updating run_N_quadrature

--- SAN/code/network_simulations/run_N_quadrature.sh
+++ SAN/code/network_simulations/run_N_quadrature.sh
@@ -1,5 +1,5 @@
-for i in {2..3}
+for i in {3..15}
 do
     echo "Running for N = $i"
-    sh run_batch_quadrature.sh quadrature_test.jl s3://ran-s3-shared/benchmark/ $i
+    sh run_batch_quadrature.sh quadrature_timing.jl s3://ran-s3-shared/benchmark/ $i
 done
