f79f53b2d37029bb091be629653fd0bfeddcd902
smitch151 <52943255+smitch151@users.noreply.github.com>
2021-04-29 09:17:57 -0400

    Update gpuify_comparison.jl

--- SAN/code/network_simulations/gpuify_comparison.jl
+++ SAN/code/network_simulations/gpuify_comparison.jl
@@ -5,60 +5,100 @@ using Cuba, Distributions
 using BenchmarkTools, Test, CUDA
 using FLoops, FoldsCUDA
 using SpecialFunctions
+using Referenceables: referenceable
 
 @test Threads.nthreads()>1
 
-M= 25 # number of independent uniform random variables
+# User Inputs
+M= 5 # number of independent uniform random variables
 atol=1e-6
 rtol=1e-3
-nvec=1000000
+nvec=10000
 maxevals=100000000
 
-x = rand(M, nvec)
-x32 = convert(Matrix{Float32},x)
-cux = CuArray(x)
-cux32 = CuArray(x32)
-
-@benchmark reduce(*, cux32, dims = 1)
-@benchmark prod(cux32, dims=1) #identical to above 
-
-function beta_pdf(x, a, b)
-     prod((x.^(a-1.0) .* (ones(M) .- x).^(b-1.0))./(gamma(a)*gamma(b)/gamma(a+b)),dims = 1)
-end
+# Initializing Matrices
+x = CuArray(rand(Float32, (M, nvec)))
+ones_mat = CuArray(ones(Float32, M))
 
 function beta_pdf_gpu(x, a, b)
-     prod(x.^(a-1.0) .* (CuArray(ones(M)) .- x).^(b-1.0)./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
-end
-
-function beta_pdf_gpu_ones(x, a, b)
-     prod(x.^(a-1.0) .* (ones_mat .- x).^(b-1.0)./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
-end
-
-function beta_pdf_gpu_ones_32(x, a, b)
      prod(x.^(a-1.0f0) .* (ones_mat .- x).^(b-1.0f0)./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
 end
 
-function beta_pdf_gpu_ones_32_sync(x, a, b)
-    CUDA.@sync prod(x.^(a-1.0f0) .* (ones_mat .- x).^(b-1.0f0)./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
+function beta_pdf_gpu_reduce(x, a, b)
+    @floop for i = 1:size(x,2)
+        reduce(*,x[:,i].^(a-1.0f0) .* (ones_mat .- x[:,i]).^(b-1.0f0)./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
+    end
 end
 
-function beta_pdf_gpu_ones_32_inbounds(x, a, b)
-   @inbounds prod(x.^(a-1.0f0) .* (ones_mat .- x).^(b-1.0f0)./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
+function beta_pdf_gpu_array(x, a, b)
+   prod(x.^(CuArray([a])-CuArray([1.0f0])) .* (ones_mat .- x).^(CuArray([b])-CuArray([1.0f0]))./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
 end
 
+function beta_pdf_gpu_precompile(x, a, b)
+    power1 = a-CuArray([1.0f0])
+    power2 = b-CuArray([1.0f0])
+    denominator = exp.(CUDA.lgamma.(a)) .* exp.(CUDA.lgamma.(b)) ./ exp.(CUDA.lgamma.(a+b))
+    prod((x.^(power1) .* (ones_mat .- x).^(power2))./denominator,dims=1)
+end
+
+
+function beta_pdf_gpu_precompile_flip(x, a, b)
+    power1 = a-CuArray([1.0f0])
+    power2 = b-CuArray([1.0f0])
+    denominator = exp.(CUDA.lgamma.(a)) .* exp.(CUDA.lgamma.(b)) ./ exp.(CUDA.lgamma.(a+b))
+    ones_mat_flip = ones_mat'
+    prod((x.^(power1) .* (ones_mat_flip .- x).^(power2))./denominator,dims=2)
+end
+
+function beta_pdf_gpu_precompile_floop(x, a, b) # super slow
+    power1 = a-CuArray([1.0f0])
+    power2 = b-CuArray([1.0f0])
+    denominator = exp.(CUDA.lgamma.(a)) .* exp.(CUDA.lgamma.(b)) ./ exp.(CUDA.lgamma.(a+b))
+    for j in 1:size(x,2)
+        @floop for w in referenceable(x[:,j])
+            @reduce(xprod *= (w[]^(power1[1]) * (1.0f0 .- w[])^(power2[1]))/denominator)
+        end
+        f[j] = xprod
+    end
+    return f
+end
+
+
+function beta_pdf_gpu_precompile_floop2(x, a, b) # super slow
+    power1 = a-CuArray([1.0f0])
+    power2 = b-CuArray([1.0f0])
+    denominator = exp.(CUDA.lgamma.(a)) .* exp.(CUDA.lgamma.(b)) ./ exp.(CUDA.lgamma.(a+b))
+    @floop CUDAEx() for j in 1:size(x,2)
+        xprod = prod((x[:,j].^(power1) .* (ones_mat .- x[:,j]).^(power2))./denominator,dims=1)
+        f[j] = xprod[1]
+    end
+    return f
+end
 
 # Benchmarking Different Options
-display(@benchmark pdf(Product(Beta.(1.0,2.0*ones(M))),$x)) # running using Beta function from Dsitributions (slowest)
-display(@benchmark beta_pdf($x, 1.0,2.0)) #running on cpu using user written Beta function
-display(@benchmark beta_pdf($x32, 1.0f0,2.0f0)) #running on cpu using user written Beta function with float 32 (no change)
-display(@benchmark beta_pdf_gpu($cux,1.0,2.0)) # running on gpu using user written beta function (faster than cpu)
+display(@benchmark beta_pdf_gpu($x, 1.0f0, 2.0f0))
+display(@benchmark beta_pdf_gpu_array($x,1.0f0,2.0f0)) # running on gpu using user written beta function with float 32 and preallocated ones as float32 (faster)
+display(@benchmark beta_pdf_gpu_reduce($x,1.0f0,2.0f0)) # slower
+display(@benchmark beta_pdf_gpu_precompile($x,CuArray([1.0f0]),CuArray([2.0f0]))) #faster 
+display(@benchmark beta_pdf_gpu_precompile_flip($x',CuArray([1.0f0]),CuArray([2.0f0]))) #marginally slower 
 
-ones_mat = CuArray(ones(M))
-display(@benchmark beta_pdf_gpu_ones($cux,1.0,2.0)) # running on gpu, user written, preallocated ones (faster than without preallocation)
+f = ones(Float32, (1, size(x,2)))
+display(@benchmark beta_pdf_gpu_precompile_floop($x,CuArray([1.0f0]),CuArray([2.0f0]))) 
+display(@benchmark beta_pdf_gpu_precompile_floop2($x,CuArray([1.0f0]),CuArray([2.0f0]))) #much slower
+display(@benchmark beta_pdf_gpu_ones_32_array_lgamma($cux32,CuArray([1.0f0]),CuArray([2.0f0]))) # running on gpu using user written beta function with float 32 and preallocated ones as float32 and inbounds (same as above) 
 
-ones_mat = CuArray(convert(Vector{Float32},ones(M)))
-display(@benchmark beta_pdf_gpu_ones_32($cux32,1.0f0,2.0f0)) # running on gpu using user written beta function with float 32 and preallocated ones as float32 (faster)
-display(@benchmark beta_pdf_gpu_ones_32_sync($cux32,1.0f0,2.0f0)) # running on gpu using user written beta function with float 32 and preallocated ones as float32 and sync (same as above) 
-display(@benchmark beta_pdf_gpu_ones_32_inbounds($cux32,1.0f0,2.0f0)) # running on gpu using user written beta function with float 32 and preallocated ones as float32 and inbounds (same as above) 
+## Trying out multiplication strategies  
+display(@benchmark reduce(*, x, dims = 1))
+display(@benchmark prod(x, dims=1)) #identical to above 
 
-
+function parallel_multi(x)
+    for i in 1:size(x, 2)
+        @floop CUDAEx() for w in referenceable(x[:,i])
+         @reduce(val *= w[])
+        end
+        result[i] = val
+    end
+    return result 
+end
+result = CUDA.ones(size(x,2),1)
+display(@benchmark parallel_multi(x)) #seems way slower
