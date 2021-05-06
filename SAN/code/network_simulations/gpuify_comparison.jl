ff786605d4fafea364a434683245d108825d8a75
smitch151 <52943255+smitch151@users.noreply.github.com>
2021-05-06 16:21:19 -0400

    Update gpuify_comparison.jl

--- SAN/code/network_simulations/gpuify_comparison.jl
+++ SAN/code/network_simulations/gpuify_comparison.jl
@@ -5,87 +5,22 @@ using Cuba, Distributions
 using BenchmarkTools, Test, CUDA
 using FLoops, FoldsCUDA
 using SpecialFunctions
-using Referenceables: referenceable
 
 @test Threads.nthreads()>1
 
 # User Inputs
-M= 36 # number of independent uniform random variables
+M= 10 # number of independent uniform random variables
 atol=1e-6
 rtol=1e-3
-nvec=100000
+nvec=1000000
 maxevals=100000000
 
 # Initializing Matrices
 x = CuArray(rand(Float32, (M, nvec)))
 ones_mat = CuArray(ones(Float32, M))
+x_cpu = rand(Float32, (M, nvec))
 
-function beta_pdf_gpu(x, a, b)
-     prod(x.^(a-1.0f0) .* (ones_mat .- x).^(b-1.0f0)./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
-end
-
-function beta_pdf_gpu_reduce(x, a, b)
-    @floop for i = 1:size(x,2)
-        reduce(*,x[:,i].^(a-1.0f0) .* (ones_mat .- x[:,i]).^(b-1.0f0)./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
-    end
-end
-
-function beta_pdf_gpu_array(x, a, b)
-   prod(x.^(CuArray([a])-CuArray([1.0f0])) .* (ones_mat .- x).^(CuArray([b])-CuArray([1.0f0]))./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
-end
-
-function beta_pdf_gpu_precompile(x, a, b)
-    power1 = a-CuArray([1.0f0])
-    power2 = b-CuArray([1.0f0])
-    denominator = exp.(CUDA.lgamma.(a)) .* exp.(CUDA.lgamma.(b)) ./ exp.(CUDA.lgamma.(a+b))
-    prod((x.^(power1) .* (ones_mat .- x).^(power2))./denominator,dims=1)
-end
-
-
-function beta_pdf_gpu_precompile_flip(x, a, b)
-    power1 = a-CuArray([1.0f0])
-    power2 = b-CuArray([1.0f0])
-    denominator = exp.(CUDA.lgamma.(a)) .* exp.(CUDA.lgamma.(b)) ./ exp.(CUDA.lgamma.(a+b))
-    ones_mat_flip = ones_mat'
-    prod((x.^(power1) .* (ones_mat_flip .- x).^(power2))./denominator,dims=2)
-end
-
-function beta_pdf_gpu_precompile_floop(x, a, b) # super slow
-    power1 = a-CuArray([1.0f0])
-    power2 = b-CuArray([1.0f0])
-    denominator = exp.(CUDA.lgamma.(a)) .* exp.(CUDA.lgamma.(b)) ./ exp.(CUDA.lgamma.(a+b))
-    for j in 1:size(x,2)
-        @floop CUDAEx() for w in referenceable(x[:,j])
-            @reduce(xprod *= w[]^power1[1] * (1.0f0 - w[])^power2[1]/denominator[1])
-        end
-        f[j] = xprod
-    end
-    return f
-end
-
-function beta_pdf_gpu_precompile_floop2(f, x, a, b) # super slow
-    power1 = a-CuArray([1.0f0])
-    power2 = b-CuArray([1.0f0])
-    denominator = (exp.(CUDA.lgamma.(a)) .* exp.(CUDA.lgamma.(b)) ./ exp.(CUDA.lgamma.(a+b)))[1]
-    @floop CUDAEx() for i in 1:size(x,2)
-        f[i] = reduce(*,(x[:,i].^(power1) .* (ones_mat .- x[:,i]).^(power2))./denominator)
-        end
-    return f
-end
-
-# Benchmarking PDF Calculations
-display("Benchmarking PDF Calculations")
-display(@benchmark beta_pdf_gpu($x, 1.0f0, 2.0f0))
-display(@benchmark beta_pdf_gpu_array($x,1.0f0,2.0f0)) 
-display(@benchmark beta_pdf_gpu_reduce($x,1.0f0,2.0f0)) # slower
-display(@benchmark beta_pdf_gpu_precompile($x,CuArray([1.0f0]),CuArray([2.0f0]))) #faster 
-display(@benchmark beta_pdf_gpu_precompile_flip($x',CuArray([1.0f0]),CuArray([2.0f0]))) #marginally slower 
-
-f = ones(Float32, (1, size(x,2)))
-display(@benchmark beta_pdf_gpu_precompile_floop($x,CuArray([1.0f0]),CuArray([2.0f0]))) #much slower
-#display(@benchmark beta_pdf_gpu_precompile_floop2($x,CuArray([1.0f0]),CuArray([2.0f0]))) # doesn't work yet
-
-## Trying out multiplication strategies  
+# Comparing Multiplication Techniques
 function parallel_multi(f, x)
    @floop CUDAEx() for i in 1:size(x, 2)
         val = reduce(*,@view(x[:,i]))
@@ -97,5 +32,48 @@ end
 result = CUDA.ones(size(x,2),1)
 display("Comparing Multiplication Methods")
 display(@benchmark parallel_multi(result, $x)) #is 2-5x faster
-display(@benchmark reduce(*, $x, dims = 1))
+display(@benchmark reduce(*, $x, dims = 1)) #2-5x slower
 display(@benchmark prod($x, dims=1)) #identical to above 
+
+### takeaway: want to use gpu for parallelization of multiplication 
+
+# Comparing Computing PDFs
+function beta_pdf_cpu(x,a,b)
+    pdf(Product(Beta.(a,b*ones(M))),x)
+end
+
+
+function beta_pdf_gpu(x, a, b)
+     prod(x.^(a-1.0f0) .* (ones_mat .- x).^(b-1.0f0)./(gamma(a)*gamma(b)/gamma(a+b)),dims=1)
+end
+
+function beta_pdf_gpu_precompile(x, a, b)
+    power1 = a-1.0f0
+    power2 = b-1.0f0
+    denominator = exp.(CUDA.lgamma.(a)) .* exp.(CUDA.lgamma.(b)) ./ exp.(CUDA.lgamma.(a+b))
+    prod((x.^(power1) .* (ones_mat .- x).^(power2))./denominator,dims=1)
+end
+
+function beta_pdf_gpu_precompile_floop(f,x, a, b) # works with CUDAEx() but slightly slower cause of calculations outside of floop loop
+    power1 = a-1.0f0
+    power2 = b-1.0f0
+    denominator = (exp.(CUDA.lgamma.(a)) .* exp.(CUDA.lgamma.(b)) ./ exp.(CUDA.lgamma.(a+b)))[1]
+    ones_mat = CuArray(ones(Float32, (M,nvec)))
+    val2 = (x.^(power1) .* (ones_mat .- x).^(power2))./denominator
+    @floop CUDAEx() for i in 1:size(x,2)
+        #f[i] = reduce(*,(x[:,i].^(power1) .* (ones_mat .- x[:,i]).^(power2))./denominator)
+        val = reduce(*,@view(val2[:,i]))
+        f[i] = val
+    end
+    return f
+end
+
+# Benchmarking PDF Calculations
+display("Comparing PDF Calculations")
+display(@benchmark beta_pdf_cpu($x_cpu, 1.0f0, 2.0f0))
+display(@benchmark beta_pdf_gpu($x, 1.0f0, 2.0f0)) #10-15x faster than cpu
+display(@benchmark beta_pdf_gpu_precompile($x,1.0f0,2.0f0)) #same as above 
+result = CUDA.ones(Float32, (size(x,2),1))
+display(@benchmark beta_pdf_gpu_precompile_floop($result, $x,1.0f0, 2.0f0)) #a bit slower than aboveh slower
+
+### Takeaway: Want to parallelize the pdf computation using floop with GPU, but have to figure out how to also do calculations with it to get gain from multiplication
