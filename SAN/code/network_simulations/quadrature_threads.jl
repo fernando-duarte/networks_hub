ee600848f761b7c1118c7ce23093d4c66f2976c4
smitch151 <52943255+smitch151@users.noreply.github.com>
2021-05-21 16:46:13 -0400

    NC-80 Updating Quadrature Threads with GPU Results

--- SAN/code/network_simulations/quadrature_threads.jl
+++ SAN/code/network_simulations/quadrature_threads.jl
@@ -3,22 +3,52 @@ Pkg.activate("joint_timing")
 Pkg.instantiate()
 
 using Cuba, Distributions
-using BenchmarkTools, Test
+using BenchmarkTools, Test, CUDA
+using SpecialFunctions
+using Suppressor
+using FLoops
 
 # Inputs
-M= 15 # number of independent beta random variables
+M= 5 # number of independent beta random variables
 atol=1e-6
 rtol=1e-3
 
 global num_evals = 1 # shows x if 1, doesn't otherwise
 
 # integrate the pdf of the joint distribution -- should always equal 1
+
+function beta_pdf_d(x::CuArray,a,b,dim)
+    prod(x.^(a-1.0f0) .* (1.0f0 .- x).^(b-1.0f0)./(gamma(a)*gamma(b)/gamma(a+b)),dims=dim)
+end
+
+function int_d(x,f)
+    if num_evals == 1 
+        @show(size(x))
+    end
+    global num_evals += 1
+    x = convert(CuArray{Float32},x)
+    val = beta_pdf_d(x, 1.0f0, 2.0f0,1)
+    f[1] = val
+end
+
+function int_thread_col_d(x,f)
+    if num_evals == 1 
+        @show(size(x))
+    end
+    global num_evals += 1
+    x_d = convert(CuArray{Float32},x)
+    val = beta_pdf_d(x_d, 1.0f0, 2.0f0,1)
+    @floop for i in 1:size(x,2)
+        f[i] = val[i]
+    end
+end
+
 function int(x, f)
     if num_evals == 1 
         @show(size(x))
     end
     global num_evals += 1
-   f[1] = pdf(Product(Beta.(1.0,2.0*ones(M))),x)
+   f[1] = pdf(Product(Beta.(1.0,2.0*ones(M))),x)[1]
 end
 
 # multithread
@@ -48,77 +78,141 @@ end
 
 # we get the right answer
 @show result, err = cuhre(int, M, 1, atol=atol, rtol=rtol)
+@suppress begin
+    display(cuhre(int_thread_col_d, M, 1, atol=atol, rtol=rtol, nvec = 100))
+end
 @show result, err = cuhre(int_thread_col, M, 1, atol=atol, rtol=rtol,nvec=100)
 @show result, err = cuhre(int_thread_el, M, 1, atol=atol, rtol=rtol,nvec=100)
 
-@btime cuhre($(int), $M, 1, atol=$atol, rtol=$rtol) # slow
+@btime cuhre($(int), $M, $1, atol=$atol, rtol=$rtol) # slow
 
 println("MultiThreading with Cuhre")
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(16)) #size is 16 for M = 5, size is 16 for M = 15,
+@btime cuhre($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16)) #size is 16 for M = 5, size is 16 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(100)) #size is 100 for M = 5, size is 100 for M = 15,
+@btime cuhre($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(100)) #size is 100 for M = 5, size is 100 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(1000)) #size is 273 for M = 5, size is 1000 for M = 15,
+@btime cuhre($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000)) #size is 273 for M = 5, size is 1000 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(10000)) #size is 273 for M = 5, size is 10000 for M = 15, 
+@btime cuhre($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000)) #size is 273 for M = 5, size is 10000 for M = 15, 
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(100000)) #size is 273 for M = 5, size is 37789 for M = 15,
+@btime cuhre($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(100000)) #size is 273 for M = 5, size is 37789 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(1000000)) #size is 273 for M = 5, size is 37789 for M = 15,
+@btime cuhre($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000000)) #size is 273 for M = 5, size is 37789 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(10000000)) #size is 273 for M = 5,size is 37789 for M = 15,
+@btime cuhre($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000000000)) #size is 273 for M = 5,size is 37789 for M = 15, size is 1,060,137 for M = 20, size is 10000000 for M = 25
+
+@suppress begin 
+global num_evals = 1 #resetting so it shows size(x)
+display(@benchmark cuhre($int_thread_col_d, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16))) #size is 16 for M = 5, size is 16 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+display(@benchmark cuhre($int_thread_col_d, $M, $1, atol=$atol, rtol=$rtol,nvec=$(100))) #size is 100 for M = 5, size is 100 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+display(@benchmark  cuhre($int_thread_col_d, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000))) #size is 273 for M = 5, size is 1000 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+display(@benchmark  cuhre($int_thread_col_d, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000))) #size is 273 for M = 5, size is 10000 for M = 15, 
+global num_evals = 1 #resetting so it shows size(x)
+display(@benchmark  cuhre($int_thread_col_d, $M, $1, atol=$atol, rtol=$rtol,nvec=$(100000))) #size is 273 for M = 5, size is 37789 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+display(@benchmark  cuhre($int_thread_col_d, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000000))) #size is 273 for M = 5, size is 37789 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+display(@benchmark  cuhre($int_thread_col_d, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000000000))) #size is 273 for M = 5,size is 37789 for M = 15,
+end
 
 println("MultiThreading with Suave")
 global num_evals = 1 #resetting so it shows size(x)
-@btime suave($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(16)) #size is 16 for M = 5, size is 16 for M = 15,
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16)) #size is 16 for M = 5, size is 16 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime suave($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(100)) #size is 100 for M = 5, size is 100 for M = 15,
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(100)) #size is 100 for M = 5, size is 100 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime suave($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(1000)) #size is 1000 for M = 5, size is 1000 for M = 15,
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000)) #size is 1000 for M = 5, size is 1000 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime suave($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(10000)) #size is 1000 for M = 5, size is 1000 for M = 15, 
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000)) #size is 1000 for M = 5, size is 1000 for M = 15, 
 global num_evals = 1 #resetting so it shows size(x)
-@btime suave($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(100000)) #size is 1000 for M = 5, size is 1000 for M = 15,
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(100000)) #size is 1000 for M = 5, size is 1000 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime suave($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(1000000)) #size is 1000 for M = 5, size is 1000 for M = 15,
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000000)) #size is 1000 for M = 5, size is 1000 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime suave($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(10000000)) #size is 1000 for M = 5,size is 1000 for M = 15,
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000000)) #size is 1000 for M = 5,size is 1000 for M = 15,
 
+println("MultiThreading with Suave by MaxEvals")
+global num_evals = 1 #resetting so it shows size(x)
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16), maxevals=$(10000)) #size is 16 for M = 5, size is 16 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16), maxevals=$(10000000)) #size is 16 for M = 5, size is 16 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16), maxevals=$(1000000000)) #size is 16 for M = 5, size is 16 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000), maxevals=$(10000)) #size is 1000 for M = 5, size is 16 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000), maxevals=$(10000000)) #size is 1000 for M = 5, size is 16 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+@btime suave($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000), maxevals=$(1000000000)) #size is 1000 for M = 5, size is 16 for M = 15,
 
 println("MultiThreading with Divonne")
 global num_evals = 1 #resetting so it shows size(x)
-@btime divonne($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(16)) #size is 24 for M = 5, size is 16 for M = 15,
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16)) #size is 24 for M = 5, size is 16 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime divonne($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(100)) #size is 24 for M = 5, size is 24 for M = 15,
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(100)) #size is 24 for M = 5, size is 24 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime divonne($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(1000)) #size is 24 for M = 5, size is 24 for M = 15,
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000)) #size is 24 for M = 5, size is 24 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime divonne($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(10000)) #size is 24 for M = 5, size is 24 for M = 15, 
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000)) #size is 24 for M = 5, size is 24 for M = 15, 
 global num_evals = 1 #resetting so it shows size(x)
-@btime divonne($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(100000)) #size is 24 for M = 5, size is 24 for M = 15,
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(100000)) #size is 24 for M = 5, size is 24 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime divonne($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(1000000)) #size is 24 for M = 5, size is 24 for M = 15,
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000000)) #size is 24 for M = 5, size is 24 for M = 15,
 global num_evals = 1 #resetting so it shows size(x)
-@btime divonne($int_thread_col, $M, 1, atol=$atol, rtol=$rtol,nvec=$(10000000)) #size is 24 for M = 5,size is 24 for M = 15,
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000000)) #size is 24 for M = 5,size is 24 for M = 15,
+
+println("MultiThreading with Divonne by MaxEvals")
+global num_evals = 1 #resetting so it shows size(x)
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16), maxevals=$(100)) #size is 16 for M = 5, size is 16 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16), maxevals=$(100000)) #size is 16 for M = 5, size is 16 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16), maxevals=$(10000000)) #size is 16 for M = 5, size is 16 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000), maxevals=$(100)) #size is 24 for M = 5, size is 24 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000), maxevals=$(100000)) #size is 24 for M = 5, size is 24 for M = 15,
+global num_evals = 1 #resetting so it shows size(x)
+@btime divonne($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000), maxevals=$(10000000)) #size is 24 for M = 5, size is 24 for M = 15,
 
 println("multithread and create product in loop")
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_el, $M, 1, atol=$atol, rtol=$rtol,nvec=$(16)) #size is 16 for M = 5
+@btime cuhre($int_thread_el, $M, $1, atol=$atol, rtol=$rtol,nvec=$(16)) #size is 16 for M = 5
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_el, $M, 1, atol=$atol, rtol=$rtol,nvec=$(100)) #size is 100 for M = 5
+@btime cuhre($int_thread_el, $M, $1, atol=$atol, rtol=$rtol,nvec=$(100)) #size is 100 for M = 5
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_el, $M, 1, atol=$atol, rtol=$rtol,nvec=$(1000)) #size is 273 for M = 5
+@btime cuhre($int_thread_el, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000)) #size is 273 for M = 5, 100 for M = 15
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_el, $M, 1, atol=$atol, rtol=$rtol,nvec=$(10000)) #size is 273 for M = 5
+@btime cuhre($int_thread_el, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000)) #size is 273 for M = 5, 10000 for M = 15
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_el, $M, 1, atol=$atol, rtol=$rtol,nvec=$(100000)) #size is 273 for M = 5
+@btime cuhre($int_thread_el, $M, $1, atol=$atol, rtol=$rtol,nvec=$(100000)) #size is 273 for M = 5, 37789 for M =15 
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_el, $M, 1, atol=$atol, rtol=$rtol,nvec=$(1000000)) #size is 273 for M = 5
+@btime cuhre($int_thread_el, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000000)) #size is 273 for M = 5, 37789 for M =15 
 global num_evals = 1 #resetting so it shows size(x)
-@btime cuhre($int_thread_el, $M, 1, atol=$atol, rtol=$rtol,nvec=$(10000000)) #size is 273 for M = 5
+@btime cuhre($int_thread_el, $M, $1, atol=$atol, rtol=$rtol,nvec=$(10000000)) #size is 273 for M = 5, 37789 for M =15 
 global num_evals = 1 #resetting so it shows size(x)
 
-## Int_thread_el and Int_thread_col have same size(x) for given M and nvec
 ## Takeaway speed increases with nvec for cuhre because the size of the x matrix grows from 16 to 30k for M = 15. 
-## Takeaway, Speed seems to increase from nvec = 16 to nvec = 100 to nvec = 1000 for Suave and Divonne, but after that remains constant as size of X maxes out at like 250. 
+## Takeaway, Speed seems to increase from nvec = 16 to nvec = 100 to nvec = 1000 for Suave and Divonne, but after that remains constant
+
+## Running for large M 
+
+M = 25
+# GPU
+@suppress begin 
+    global num_evals = 1
+    display(@benchmark  cuhre($int_thread_col_d, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000000000))) 
+end
+# M = 5 15 ms; M = 10 130 ms; M = 15 1.9s; M = 20 19.2s; M = 25 530 s (on p2)
+# M = 5 11 ms; M = 10 100 ms; M = 15 1.5s; M = 20 14.8s; M = 25 400 s; (on p3)
+
+
+# Cpu
+global num_evals = 1
+display(@benchmark  cuhre($int_thread_col, $M, $1, atol=$atol, rtol=$rtol,nvec=$(1000000000))) 
+# For M = 5 1.5ms, M = 10 13ms,  M = 15 260 ms, M = 20 9s, M =25 470s (on p2)
+# For M = 5 1.5ms, M = 10 41ms,  M = 15 260 ms, M = 20 8s, M =25 390s(on p3)
