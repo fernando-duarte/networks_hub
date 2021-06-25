e8f89677f2bff9311892cbab41d31473e1bcd558
smitch151 <52943255+smitch151@users.noreply.github.com>
2021-06-25 11:56:41 -0400

    NC-93 Adding new A generation and tests

--- SAN/code/network_simulations/surrogates_NN_Axc.jl
+++ SAN/code/network_simulations/surrogates_NN_Axc.jl
@@ -8,10 +8,16 @@ using Flux.Data: DataLoader
 using IterTools: ncycle
 using Flux: throttle
 using ForwardDiff
+using Convex, SCS, LinearAlgebra, Distributions, Test, Random, MathOptInterface, Parameters, Suppressor
+const MOI = MathOptInterface
+include("generateA.jl")
+using .generateA
+
 
 # User Inputs
 N = 5 #number of points
 Q = 1000 #number of samples
+max_iter = 10000
 
 # Loading Data
 xf = XLSX.readxlsx("node_stats_forsimulation_all.xlsx") 
@@ -20,11 +26,9 @@ unique!(data) # delete duplicate rows, use `nonunique(data)` to see if there are
 data = data[isequal.(data.qt_dt,195), :] # keep quarter == 195 = 2008q4
 sort!(data, :assets, rev = true)
 data = data[1:N,:] # keep small number of nodes, for testing
-#N = size(data,1) # number of nodes
 units = 1e6;
 data[:,[:w, :c, :assets, :p_bar, :b]] .= data[!,[:w, :c, :assets, :p_bar, :b]]./units
-# data.b[:] .= missing
-# data.c[:] .= missing
+
 
 col_with_miss = names(data)[[any(ismissing.(col)) for col = eachcol(data)]] # columns with at least one missing
 data_nm = coalesce.(data, data.assets/1.5) # replace missing by a value
@@ -102,7 +106,14 @@ function gen_A(N,Q)
     end
     return A_in
 end
+
 A_in = gen_A(N,Q)
+A_in_alt = trainingSetA(N,sample_size=Int(Q/2),max_iter=max_iter,con="netting") # N=5 nodes, can use for training NN
+for i = 1:N^2
+    for j = 1:Int(Q/2)
+        A_in[i,j] = vcat(A_in_alt[j]...)[i]
+    end
+end
 
 # Making c
 function gen_c(N,Q)
@@ -115,6 +126,19 @@ function gen_c(N,Q)
     return c_in
 end
 c_in = gen_c(N,Q)
+
+
+# Making b
+function gen_b(N,Q)
+    b_in = zeros(N,Q)
+    for i = 1:Q
+        b_in[:,i] = b0 .+ ((rand(N) .- 0.5)./5) #slight permutations to c
+    end
+    ind = b_in .< 0 
+    b_in[ind] .= 0.0
+    return b_in
+end
+b_in = gen_b(N,Q)
 
 # Function to replicate
 function clearing_vec!(F, p, x, p_bar, A, c)
@@ -129,20 +153,21 @@ function p_func(x, p_bar, A, c)
 end
 
 ## Training NN
-m = Int(Q/10) #size of test set
-x_train = vcat(A_in, c_in, x_in) #order is A, b,c,x
-x_test = x_train[:,Q-m+1:end]
-x_train = x_train[:,1:Q - m]
+m = Int(Q/10) #number of observations in test set
+x_train = vcat(A_in, b_in, c_in, x_in) #order is A, b,c,x
+x_train = x_train[:,randperm(Q)] #randomly sort columns of X_train
+x_test = x_train[:,1:m]
+x_train = x_train[:,m+1:end]
 y_train = zeros(N,Q-m)
 y_test = zeros(N,m)
 for i = 1:Q-m
-    y_train[:,i] = p_func(x_train[N*N+N:end,i],p_bar,reshape(x_train[1:N*N,i], (N,N)),x_train[N*N+1:N*N+N,i]) 
+    y_train[:,i] = p_func(x_train[N*N+2*N+1:end,i],p_bar,reshape(x_train[1:N*N,i], (N,N)),x_train[N*N+N+1:N*N+2*N,i])  #x, p_bar, A, c
 end
 for i = 1:m
-    y_test[:,i] = p_func(x_test[N*N+N:end,i],p_bar,reshape(x_test[1:N*N,i], (N,N)),x_test[N*N+1:N*N+N,i]) 
+    y_test[:,i] = p_func(x_test[N*N+2*N+1:end,i],p_bar,reshape(x_test[1:N*N,i], (N,N)),x_test[N*N+N+1:N*N+2*N,i]) 
 end
 
-model = Chain(Dense(N*N+2*N, 50, σ), Dense(50,20,σ), Dense(20,10,σ), Dense(10,5,σ), Dense(5, N))
+model = Chain(Dense(N*N+3*N, 50, σ), Dense(50,20,σ), Dense(20,10,σ), Dense(10,5,σ), Dense(5, N))
 loss(x, y) = Flux.mse(model(x), y)
 
 ps = Flux.params(model) #parameters that update
@@ -157,22 +182,56 @@ evalcb() = @show(loss(x_test,y_test))
 init_loss = loss(x_test,y_test)
 
 ## Training
-Flux.@epochs 5 Flux.train!(loss, ps, ncycle(train_loader, 10), opt, cb  = throttle(evalcb,50))
+Flux.@epochs 150 Flux.train!(loss, ps, ncycle(train_loader, 10), opt, cb  = throttle(evalcb,50))
 
 # Computing Jacobian and Hessian
 j = x -> ForwardDiff.jacobian(model, x)
 j(x_test[:,1])
 
 ## Creating many functions for each output of the NN
-function model1(x) 
-    model(x)[1]
-end
-function model2(x) 
-    model(x)[2]
+function model_hessian(x) 
+    sum(model(x))
 end
 
-h1 = x -> ForwardDiff.hessian(model1, x)
-h1(x_test[:,1])
-h2 = x -> ForwardDiff.hessian(model2, x)
-h2(x_test[:,1])
+h_model = x -> ForwardDiff.hessian(model_hessian, x)
+h_model(x_test[:,1])
 
+function p_func2(w)
+    x = w[N*N+2*N+1:end]
+    A = reshape(w[1:N*N],(N,N))
+    c = w[N*N+N+1:N*N+2*N]
+  sol = nlsolve((F, p)->clearing_vec!(F, p, x, p_bar, A, c),[p_bar...], autodiff = :forward)
+  return sol.zero; # the solution to the system
+end
+
+function p_hessian(x) 
+    sum(p_func2(x))
+end
+
+h_p = x -> ForwardDiff.hessian(p_hessian, x)
+p_hessian(x_test[:,1])
+h_p(x_test[:,1]) #doesn't work unclear why
+
+@testset "Neural Net Check" begin
+    W = 100 #size of test set
+    c_in = gen_c(N,W)
+    b_in = gen_b(N,W)
+    x_in = gen_x(N,W,α0,β0)
+    
+    A_in = gen_A(N,W)
+    A_in_alt = trainingSetA(N,sample_size=Int(W/2),max_iter=max_iter,con="netting") # N=5 nodes, can use for training NN
+    for i = 1:N^2
+        for j = 1:Int(W/2)
+           A_in[i,j] = vcat(A_in_alt[j]...)[i]
+        end
+    end
+    x_test = vcat(A_in, b_in, c_in, x_in) #order is A, b,c,x
+    x_test = x_test[:,randperm(W)] #randomly sort columns of X_train
+    y_test = zeros(N,W)
+    for i = 1:W
+        y_test[:,i] = p_func(x_test[N*N+2*N+1:end,i],p_bar,reshape(x_test[1:N*N,i], (N,N)),x_test[N*N+N+1:N*N+2*N,i]) 
+    end
+
+    @test loss(x_test,y_test) < 0.01
+      
+end;
